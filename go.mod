76c3691f5bb8d52aae8e13d6dfd391ffc3948ee5
vroyibg <vroyibg@gmail.com>
2020-09-06 19:41:50 +0700

    just a test api

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vroyibg/text-segmentation-service
 
 go 1.13
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+import "github.com/gorilla/schema"
+
+type SegmentationRequest struct {
+	TextId int64 `schema:textId`
+}
+
+type Text struct {
+	Content      string
+	LanguageCode string
+}
+
+type Error struct {
+	Message string
+}
+
+func handleSegment(w http.ResponseWriter, r *http.Request) {
+	var decoder = schema.NewDecoder()
+	var segmentationRequest SegmentationRequest
+	response := json.NewEncoder(w)
+	err := decoder.Decode(&segmentationRequest, r.URL.Query())
+	if err != nil {
+		log.Println("Error in GET parameters : ", err)
+	} else {
+		log.Println("GET parameters : ", segmentationRequest)
+	}
+
+	datasource := os.Getenv("LWT_DATASOURCE")
+	// "username:password@tcp(localhost:3306)/lwt"
+	db, err := sql.Open("mysql", datasource)
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	query := fmt.Sprintf("select Content,LanguageCode from texts where id = %d;", segmentationRequest.TextId)
+	rows, err := db.Query(query)
+	if err != nil {
+		response.Encode(Error{Message: "failed to query text."})
+		return
+	}
+	if rows.Next() {
+		var text = Text{}
+		if err := rows.Scan(&text.Content, &text.LanguageCode); err != nil {
+			response.Encode(Error{Message: "Failed to map result."})
+			return
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(text)
+			return
+		}
+	} else {
+		response.Encode(Error{Message: "Text not found."})
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/segments", handleSegment)
+	err := http.ListenAndServe(":5421", nil)
+	if err != nil {
+		panic(err)
+	}
+}
